4bbedb2ef9f6c0b774c01e089ab1edf6f4ccc734
Edson Boldrini <edsonboldrini@gmail.com>
2022-10-27 12:26:03 -0300

    First terraform state working

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "1.3.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.36.1"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "edsonboldrini-prod"
+}
+
+resource "aws_s3_bucket" "tf-test-learn-bucket" {
+  bucket = "tf-test-learn-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.tf-test-learn-bucket.id
+  acl    = "private"
+}
